01d695034174e11d537b02a04333bb64839437f9
Sumit Dhar <66067910+sumit10300203@users.noreply.github.com>
2023-08-17 22:08:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ st.header(":red[Welcome]", anchor = False)
 st.divider()
 
 if 'df' not in st.session_state:
-    st.session_state.df = pd.DataFrame(columns = ['Date', 'Time Entered', 'Last Modified', 'Occupancy'])
+    st.session_state.df = pd.DataFrame(columns = ['Time Entered', 'Last Modified', 'Occupancy'])
 
 if 'occupancy' not in st.session_state:
     st.session_state.occupancy = ''
 
 @st.cache_data
 def convert_df(df):
-    return df.to_csv().encode('utf-8')
+    return df.to_csv(index = False).encode('utf-8')
 
 
 def submit():
@@ -29,27 +29,20 @@ def submit():
     st.session_state.widget = ''
 
 def update():
-    # print(st.session_state.editeddf)
     tmp = st.session_state.editeddf['edited_rows']
     for i in tmp:
         for j in tmp[i]:
             edited_df.loc[i][j] = tmp[i][j]
-        edited_df.loc[i]['Date'] = datetime.now().date()
-        edited_df.loc[i]['Last Modified'] = datetime.now(timezone("Asia/Kolkata")).time().strftime("%H:%M:%S")
-        edited_df.loc[i]['Date'] = datetime.now(timezone("Asia/Kolkata")).date()
-
-    tmp = st.session_state.editeddf['deleted_rows']
-    for i in tmp:
-        edited_df.drop(i, axis = 0, inplace = True)
-    edited_df.reset_index(inplace = True, drop = True)
+        edited_df.loc[i]['Last Modified'] = datetime.now(timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")
+        edited_df.loc[i]['Date'] = datetime.now(timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")
 
 col = st.columns(2)
 with col[0].container():
     col[0].text_input('Enter current occupancy', key='widget', placeholder = '', on_change=submit)
     if st.session_state.occupancy:
-        st.session_state.df.loc[st.session_state.df.shape[0]] = [datetime.now(timezone("Asia/Kolkata")).date(), datetime.now(timezone("Asia/Kolkata")).time().strftime("%H:%M:%S"), datetime.now(timezone("Asia/Kolkata")).time().strftime("%H:%M:%S"), st.session_state.occupancy]
+        st.session_state.df.loc[st.session_state.df.shape[0]] = [datetime.now(timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S"), datetime.now(timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S"), st.session_state.occupancy]
         st.session_state.occupancy = ''
-    edited_df = col[0].data_editor(st.session_state.df, num_rows="fixed", key = 'editeddf', on_change = update, hide_index = True, use_container_width = True, disabled=['Date', 'Last Modified'])
+    edited_df = col[0].data_editor(st.session_state.df, num_rows="fixed", key = 'editeddf', on_change = update, hide_index = True, use_container_width = True, disabled=['Last Modified'])
     st.session_state.df = edited_df
 
 st.caption('**:red[Note:] Only Time Entered and Occupancy can be modified.**')
@@ -57,4 +50,4 @@ st.caption('**:red[Note:] Only Time Entered and Occupancy can be modified.**')
 disabled = True
 if st.session_state.df.shape[0]:
     disabled = False
-st.download_button(label="Download data as CSV", data = convert_df(st.session_state.df), file_name=f'Occupancy_{datetime.now(timezone("Asia/Kolkata")).date()}_{datetime.now(timezone("Asia/Kolkata")).time().strftime("%H:%M:%S")}.csv', mime='text/csv', disabled = disabled)
+st.download_button(label="Download data as CSV", data = convert_df(st.session_state.df), file_name=f'Occupancy_{datetime.now(timezone("Asia/Kolkata")).strftime("%Y-%m-%d_%H:%M:%S")}.csv', mime='text/csv', disabled = disabled)
